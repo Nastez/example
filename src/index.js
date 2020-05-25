03d35ccb38e07a673762f5221c2ddf7b7e46b19a
Nastez <mineevanastez@gmail.com>
2020-05-25 14:23:02 +0700

    Copied the project without js, connected plugins

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import 'paginationjs'
+import 'air-datepicker'
+import 'slick-carousel'
+import 'nouislider'
+import 'ion-rangeslider'
+
+
 import './js/common'
 import './assets/css/main.css'
 import './assets/scss/main.scss'
+
+
+import '../node_modules/air-datepicker/dist/css/datepicker.min.css'
+import '../node_modules/slick-carousel/slick/slick.css'
+import '../node_modules/slick-carousel/slick/slick-theme.css'
+
+import './assets/scss/main.scss'
+import './assets/scss/utils/ui_kit.scss'
